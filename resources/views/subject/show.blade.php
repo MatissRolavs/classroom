28117ab8e366dfe63a1583e680fef88ce77b416c
matissbby <matissrolavs2005@gmail.com>
2024-10-17 10:19:36 +0300

    inivte

--- resources/views/subject/show.blade.php
+++ resources/views/subject/show.blade.php
@@ -2,5 +2,11 @@
     <div>
         <h1>{{ $subject->name }}</h1>
         <p>{{ $subject->description }}</p>
+        <button
+            type="button"
+            onclick="this.outerHTML = '{{ $subject->code }}'"
+        >
+            Show invite code
+        </button>
     </div>
 
